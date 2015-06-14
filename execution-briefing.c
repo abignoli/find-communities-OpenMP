d5ff764e47755ed909a158ce2f5bfbcea1d94ece
andbig <andrea.bignoli@gmail.com>
2015-06-14 16:52:39 +0200

    Added features and script useful for benchmarking

--- execution-briefing.c
+++ execution-briefing.c
@@ -9,9 +9,9 @@ void execution_briefing_print(execution_briefing *briefing) {
 	printf("\tExecution successful:                                %s\n", (briefing->execution_successful ? "Yes" : "No"));
 	printf("\tNumber of algorithm runs:                            %d\n", briefing->performed_runs);
 	printf("\tAverage modularity obtained:                         %f\n", briefing->output_modularity);
-	printf("\tAveraged execution time:                             %fs\n", briefing->execution_time);
+	printf("\tAverage execution time:                              %fs\n", briefing->execution_time);
 	printf("\tMinimum execution time:                              %fs\n", briefing->minimum_execution_time);
 	printf("\tMinimum clock execution time:                        %fs\n", briefing->minimum_clock_execution_time);
-	printf("\tAveraged sum of execution time over all threads:     %fs\n", briefing->clock_execution_time);
+	printf("\tAverage sum of execution time over all threads:      %fs\n", briefing->clock_execution_time);
 	printf("\tTotal execution time:                                %fs\n", briefing->global_execution_time);
 }
