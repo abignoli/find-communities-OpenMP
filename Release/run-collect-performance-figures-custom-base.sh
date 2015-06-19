d6f6cd863bb7b5127d9a45ec9431801582dba8cb
andbig <andrea.bignoli@gmail.com>
2015-06-19 04:06:56 +0200

    Pre-measurements

--- Release/run-collect-performance-figures-custom-base.sh
+++ Release/run-collect-performance-figures-custom-base.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./collect-performance-figures-custom-base.sh ../test-graphs/cond-mat-2005.graph 2 performance/180615-0138-O3.txt 10 4
+./collect-performance-figures-custom-base.sh ../test-graphs/cnr-2000.graph 2 performance/figures-cnr-2000-190615-0354-O3.txt 2 4
